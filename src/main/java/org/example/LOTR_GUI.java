42e35467cf9e7eaf19c2314ff24e1c04fd9e27f2
Ibrahim Blake <iblake@harding.edu>
2023-04-19 18:40:24 -0500

    intiail commit

---
+++ src/main/java/org/example/LOTR_GUI.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+
+public class LOTR_GUI {
+    // MAIN FORM
+    // Name -- case insensitive just first name
+    // btn -- search
+
+    private JButton button1;
+}
---
+++ src/main/java/org/example/Request.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Request {
+    // Cache every character -> both files. || array
+    // Cache every quote -> both files. || array
+    // Return data as json.
+    // Return Specific Characters.
+    // Return Specific Quotes.
+}
